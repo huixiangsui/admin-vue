516cf7411de60c5492abe0c70ab16afc946ebf52
gaohui <1614223788@qq.com>
2018-04-10 21:04:46 +0800

    登录成功跳转到home组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,14 +1,50 @@
 <template>
 <div>
-    <p>
-      登录组件
-    </p>
+    <p>登录组件</p>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">用户名</label>
+        <input type="text"
+          id="username"
+          v-model="userForm.username"
+          >
+      </div>
+      <div>
+        <label for="username">用户名</label>
+        <input type="text"
+          id="username"
+          v-model="userForm.password"
+          >
+      </div>
+      <div>
+        <button>点击登录</button>
+      </div>
+    </form>
 </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
   data () {
-    return {}
+    return {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      const res = await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
+      // console.log(res)
+      const data = res.data
+      if (data.meta.status === 200) {
+        // this.$router.push('/')
+        this.$router.push({
+          name: 'home'
+        })
+      }
+    }
   }
 }
 </script>
